f809d1fb8fc3e0d6af3e4874172a3643b8ded628
Danny Cohen <daniel.m.cohen@berkeley.edu>
2019-02-24 03:39:07 -0500

    added some shit

--- public/js/interaction.js
+++ public/js/interaction.js
@@ -38,3 +38,18 @@ $(document).ready(function() {
         alert("Handler for .click() called.");
     });
 });
+$(document).ready(function() {
+    $("#playButton").click(function() {
+        stateMap.betterUpdate();
+    });
+});
+$(document).ready(function() {
+    $("#pauseButton").click(function() {
+        alert("pause button clicked.");
+    });
+});
+$(document).ready(function() {
+    $("#stopButton").click(function() {
+        alert("pause button clicked.");
+    });
+});
