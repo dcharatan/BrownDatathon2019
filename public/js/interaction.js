b6945cb65a0dbca71af3d1a87ba9706eb1fc4ab5
David Charatan <david.charatan@gmail.com>
2019-02-24 01:46:24 -0500

    clicky jquery thingie

---
+++ public/js/interaction.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $("#dataSelectorButton1").click(function() {
+        alert("Handler for .click() called.");
+    });
+});
+$(document).ready(function() {
+    $("#dataSelectorButton2").click(function() {
+        alert("Handler for .click() called.");
+    });
+});
+$(document).ready(function() {
+    $("#dataSelectorButton3").click(function() {
+        alert("Handler for .click() called.");
+    });
+});
+$(document).ready(function() {
+    $("#dataSelectorButton4").click(function() {
+        alert("Handler for .click() called.");
+    });
+});
+$(document).ready(function() {
+    $("#dataSelectorButton5").click(function() {
+        alert("Handler for .click() called.");
+    });
+});
+$(document).ready(function() {
+    $("#dataSelectorButton6").click(function() {
+        alert("Handler for .click() called.");
+    });
+});
+$(document).ready(function() {
+    $("#dataSelectorButton7").click(function() {
+        alert("Handler for .click() called.");
+    });
+});
+$(document).ready(function() {
+    $("#dataSelectorButton8").click(function() {
+        alert("Handler for .click() called.");
+    });
+});
